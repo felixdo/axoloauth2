2c7b5f0dc848651e364ddf09d52639a44bb3c864
Felix Dorner <felix.dorner@vorwerk.de>
2024-05-19 08:45:20 +0200

    Update build.clj

--- build.clj
+++ build.clj
@@ -5,29 +5,21 @@
             ))
 
 (def lib 'com.github.felixdo/axoloauth2)
-(def version "0.1.0-SNAPSHOT")
+(def version "0.1.0")
 #_ ; alternatively, use MAJOR.MINOR.COMMITS:
 (def version (format "1.0.%s" (b/git-count-revs nil)))
 (def class-dir "target/classes")
-(def jar-file (format "target/%s-%s.jar" (name lib) version))
-(def uber-file (format "target/%s-%s-standalone.jar" (name lib) version))
 
-;; delay to defer side effects (artifact downloads)
-(def basis (delay (b/create-basis {:project "deps.edn"})))
+(defn test "Run all the tests." [opts]
+  (let [basis    (b/create-basis {:aliases [:test]})
+        cmds     (b/java-command
+                  {:basis      basis
+                    :main      'clojure.main
+                    :main-args ["-m" "cognitect.test-runner"]})
+        {:keys [exit]} (b/process cmds)]
+    (when-not (zero? exit) (throw (ex-info "Tests failed" {}))))
+  opts)
 
-(defn clean [_]
-  (b/delete {:path "target"}))
-
-(defn jar [_]
-  (b/write-pom {:class-dir class-dir
-                :lib lib
-                :version version
-                :basis @basis
-                :src-dirs ["src"]})
-  (b/copy-dir {:src-dirs ["src" "resources"]
-               :target-dir class-dir})
-  (b/jar {:class-dir class-dir
-          :jar-file jar-file}))
 
 (defn- jar-opts [opts]
   (assoc opts
@@ -38,6 +30,22 @@
           :class-dir class-dir
           :target "target"
           :src-dirs ["src"]))
+
+(defn clean [_]
+  (b/delete {:path "target"}))
+
+(defn ci "Run the CI pipeline of tests (and build the JAR)." [opts]
+  (test opts)
+  (b/delete {:path "target"})
+  (let [opts (jar-opts opts)]
+    (println "\nWriting pom.xml...")
+    (b/write-pom opts)
+    (println "\nCopying source...")
+    (b/copy-dir {:src-dirs ["resources" "src"] :target-dir class-dir})
+    (b/copy-file {:src "LICENSE" :target (str class-dir "/LICENSE")})
+    (println "\nBuilding JAR...")
+    (b/jar opts))
+  opts)
 
 (defn install "Install the JAR locally." [opts]
   (let [opts (jar-opts opts)]
@@ -50,14 +58,15 @@
                 :pom-file (b/pom-path (select-keys opts [:lib :class-dir]))}))
   opts)
 
+
 (defn uber [_]
   (clean nil)
-  (b/copy-dir {:src-dirs ["src" "resources"]
-               :target-dir class-dir})
-  (b/compile-clj {:basis @basis
-                  :ns-compile '[axoloauth2.main]
-                  :class-dir class-dir})
-  (b/uber {:class-dir class-dir
-           :uber-file uber-file
-           :basis @basis
-           :main 'axoloauth2.main}))
+  (let [basis (b/create-basis {})]
+    (b/copy-dir {:src-dirs ["src" "resources"]
+                 :target-dir class-dir})
+    (b/compile-clj {:basis basis
+                    :ns-compile '[axoloauth2.main]
+                    :class-dir class-dir})
+    (b/uber {:class-dir class-dir
+             :uber-file (format "target/%s-%s-standalone.jar" (name lib) version)
+             :basis basis })))
